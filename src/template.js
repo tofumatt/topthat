db1c38622a1db2f2c7c62a9bb85f09d6a7be6ec7
Matthew Riley MacPherson <matt@lonelyvegan.com>
2014-04-28 11:21:29 -0400

    Use node option in jshintrc

--- src/template.js
+++ src/template.js
@@ -1,7 +1,7 @@
 // Do simple substitutions on a String object.
-module.exports = function(string, substitutions) {
-  'use strict';
+'use strict';
 
+module.exports = function(string, substitutions) {
   return string.replace(/\{\{([A-Za-z0-9]+)\}\}/g, function(match, key) {
     return substitutions[key] !== undefined ? substitutions[key] : '';
   });
